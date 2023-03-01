b8f130f3593462ca9d4e9bb422e2a591748a6f21
ElenaStr13 <estrateluk@yahoo.com>
2023-03-01 20:05:58 +0200

    react

--- script.js
+++ script.js
@@ -1,93 +1,55 @@
-/*1. Завантажити json  файл, отримати від нього дані та завантажити
-їх у функцію showAdminName(data), яка виведе ім'я адміна у консоль*/
+/*1. Існує масив імен користувачів та пустий об'єкт users.
+Використовуючи деструктуризацію, додати у об'єкт імена користувачів,
+ключами об'єкта повинні бути імена користувачів. Ви повинні отримати
+такий об'єкт*/
 
-fetch('http://127.0.0.1:5501/data.json')
-    .then(response => response.json())
-    .then(function showAdminName(data) {
-        let val = Object.values(data);
-        val.map(function (element) {
-            if (element.isAdmin == true) {
-                console.log(element.name)
-            }
-        })
-    });
+let usersNames = ['Mike', 'Bob', 'Nikola'];
+let users = {};
+let [mike, bob, nikola] = usersNames;
 
-/*2. Зробити proxy функції getFullName(), щоб не повертало underfind обгорнувши в proxy*/
-let nikola = { firstName: 'Nikola', lastName: 'Tesla' };
-let bob = { firstName: 'Bob' };
-let mike = { lastName: 'Smith' };
-let michael = {};
+users = {mike, bob, nikola};
 
-let getFullName = function (user) {
-    return `${user.firstName} ${user.lastName}`;
+console.log(users);//{mike: "Mike", bob: "Bob", nikola: "Nikola"}
+
+/*2. Існує об'єкт з заробітними платами користувачів. Створити
+функцію, яка поверне ім'я співробітника, який отримує найвищу заробітну плату.
+Використовувати Object.entries() та деструктурування для перебору пар ключ/значення*/
+
+let salaries = {
+    'Mike': 1500,
+    'Bob': 1800,
+    'Nikola': 1700
 };
 
-getFullName = new Proxy(getFullName, {
-    apply(_target, _thisArgs, args) {
-        let vall;
-        args.map((el) => vall = el);
-        if (Object.keys(vall).length === 0) {
-            return `No name`;
-        } else {
-            switch (undefined) {
-                case vall.firstName:
-                    return `${vall.lastName}`;
-                case vall.lastName:
-                    return `${vall.firstName}`;
-                default:
-                    return `${vall.firstName} ${vall.lastName}`;
-            }
+function maxSalary(salaries) {
+    let MaxSal = 0,
+        maxName;
+    for (const [name, salary] of Object.entries(salaries)) {
+        if (MaxSal < salary) {
+            MaxSal = salary;
+            maxName = name;
         }
     }
-});
+    return `Отримувач максимального окладу: ${maxName}`;
+};
+console.log(maxSalary(salaries));
 
-console.log(getFullName(bob));
-console.log(getFullName(mike));
-console.log(getFullName(michael));
-console.log(getFullName(nikola));
+/* Існує об'єкт users. Використовуючи деструктурізацію, створіть змінні userMike,
+userBob та  userNikola  і додайте в них відповідні значення об'єкта.*/
 
-/*3.Масив додати у localStorage та написати функції виводу імені по id*/
+// let users = {
+//     mike: "Mike", 
+//     bob: "Bob", 
+//     nikola: "Nikola"
+// };
 
-let users = [
-    { name: 'Nikola', age: 18, id: 1 },
-    { name: 'Bob', age: 25, id: 2 },
-    { name: 'Mike', age: 32, id: 3 },
-];
+let {mike: userMike, bob: userBob, nikola: userNikola } = users;
 
-localStorage.setItem('users', JSON.stringify(users));
-let lsObject = localStorage.getItem('users');
-let lsNewObject = JSON.parse(lsObject);
+console.log (`userMike: ${userMike}`);
+console.log (`userBob: ${userBob}`);
+console.log (`userNikola: ${userNikola}`);
 
-let arr = Object.values(lsNewObject);
-function sayHelloToUser(idUser) {
+/*4. Існують два файли з JSX  кодом App.js, встановіть у проєкт бібдіотеку
+React та запустіть ці файли, браузер повинен відобразити контент за завданням*/
 
-    arr.map((el) => {
-        if (el.id == idUser) {
-            console.log(`Hello: ${el.name}`)
-        }
-    })
-}
-sayHelloToUser(3);
-
-/*Є input. Зробити так, щоб при відкритті сторінки input містив той самий текст,
-що вів користувач минулого разу*/
-
-let textUser = document.getElementById("text-user");
-let buttonU = document.querySelector(".button");
-
-buttonU.addEventListener("click", function () {
-    let textFor = textUser.value;
-    localStorage.removeItem('user text');
-    localStorage.setItem('user text', JSON.stringify(textFor));
-    return textFor
-});
-window.addEventListener("load", function () {
-    let valf = localStorage.getItem('user text');
-    console.log(valf);
-    console.log(typeof (valf));
-    valNew = valf.replace(/[^a-zа-яё]/gi, '');
-    console.log(valNew)
-    textUser.value = valNew;
-
-});
-
+//це завдання в іншому посиланні - іншій папці
